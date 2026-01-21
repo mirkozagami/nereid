69ce00f50e3a6dbdd3c8c086a89e0fbda5692a2b
Mirko Zagami <mirko.zagami@cogitocreo.com>
2026-01-21 14:24:42 +0100

    chore: initialize Gradle project with IntelliJ Platform plugin

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.intellij.platform") version "2.2.1"
+}
+
+group = providers.gradleProperty("pluginGroup").get()
+version = providers.gradleProperty("pluginVersion").get()
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity(providers.gradleProperty("platformVersion").get())
+        bundledPlugin("com.intellij.java")
+        pluginVerifier()
+        testFramework(TestFrameworkType.Platform)
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.1")
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        name = providers.gradleProperty("pluginName")
+        version = providers.gradleProperty("pluginVersion")
+        ideaVersion {
+            sinceBuild = "233"
+            untilBuild = provider { null }
+        }
+    }
+}
+
+tasks {
+    test {
+        useJUnitPlatform()
+    }
+}
